5f39078d4d02466deb798f78fc546d6d53c5fc09
caegomezda <camiloedg23@gmail.com>
2022-06-20 03:09:32 -0500

    Correcciones estilo

--- src/app/menu-principal/menu-principal.page.ts
+++ src/app/menu-principal/menu-principal.page.ts
@@ -9,7 +9,21 @@ import { UtilitiesService } from '../service/utilities.service';
   styleUrls: ['./menu-principal.page.scss'],
 })
 export class MenuPrincipalPage implements OnInit {
-subjects;
+subjects1={
+  icon: 'assets/icon/car-sport-outline.svg',
+  name: 'Servicio de taxis',
+  id: '/principal'
+}
+subjects2={
+  icon: 'assets/icon/navigate-outline.svg',
+  name: 'Intermunicipal',
+  id: '/intermunicipal'
+}
+subjects3={
+  icon: 'assets/icon/help-outline.svg',
+  name: 'Contáctanos',
+  id: '/contact'
+}
   constructor(
     private router: Router,
     private utilities : UtilitiesService,
@@ -21,28 +35,13 @@ subjects;
   }
   
   ngOnInit() {
-    this.subjects=[
-      {
-        icon: 'assets/icon/person-outline.svg',
-        name: 'Perfil',
-        id: '/profile'
-      },
-      {
-        icon: 'assets/icon/car-sport-outline.svg',
-        name: 'Servicio de taxis',
-        id: '/principal'
-      },
-      {
-        icon: 'assets/icon/navigate-outline.svg',
-        name: 'Viaje intermunicipal',
-        id: '/intermunicipal'
-      },
-      {
-        icon: 'assets/icon/help-outline.svg',
-        name: 'Ayuda',
-        id: '/contact'
-      },
-    ];
+          // {
+      //   icon: 'assets/icon/person-outline.svg',
+      //   name: 'Perfil',
+      //   id: '/profile'
+      // },
+
+
   }
   
 
